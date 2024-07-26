b751f23cab2b680aedd329d1dda90e8a358b22d4
OhDG <ohdonggyu@naver.com>
2024-07-26 15:12:08 +0900

    feat : 로그인 페이지 UI 구현

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import './App.css';
-import Start from './pages/StartPage'
-import Main from './pages/MainPage'
+import "./App.css";
+import Start from "./pages/StartPage";
+import Main from "./pages/MainPage";
+import Login from "./pages/LoginPage";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //npm install react-router-dom
 
-
 function App() {
-  return (
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Start />} />
-                    <Route path="/main" element={<Main />} />
-                                      
-                </Routes>
-            </Router>
- 
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Start />} />
+                <Route path="/main" element={<Main />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
