afe463e5f1377e647429330e7ac349fb55132a64
seung yeon <137742302+moanuna@users.noreply.github.com>
2024-07-27 22:19:10 +0900

    feat: 게시판 작성 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Start from './pages/StartPage'
 import Main from './pages/MainPage'
+import CommunityBoard1 from './pages/CommunityBoard1.jsx';
+import CommunityBoard2 from './pages/CommunityBoard2.jsx';
+import CommunityBoard3 from './pages/CommunityBoard3.jsx';
+import CommunityBoard4 from './pages/CommunityBoard4.jsx';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //npm install react-router-dom
 
@@ -11,7 +15,10 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Start />} />
                     <Route path="/main" element={<Main />} />
-            
+                    <Route path="/CommunityBoard1" element={<CommunityBoard1 />} />   
+                    <Route path="/CommunityBoard2" element={<CommunityBoard2 />} />
+                    <Route path="/CommunityBoard3" element={<CommunityBoard3 />} />   
+                    <Route path="/CommunityBoard4" element={<CommunityBoard4 />} /> 
 
                                       
                 </Routes>
