c9cdca052106e54fbc091eb6a170d12f5dd5d19d
OhDG <ohdonggyu@naver.com>
2024-08-02 23:45:48 +0900

    feat : 기업 회원가입 페이지 UI 구현

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ContestCheck from "./pages/ContestCheck";
 import ContestCheckSchool from "./pages/ContestCheckSchool";
 import Login from "./pages/LoginPage";
 import Signin from "./pages/SigninPage";
+import SigninBusiness from "./pages/SigninBusinessPage";
 import PortfolioPage from "./pages/PortfolioPage";
 import IntroducePage from "./pages/IntroducePage";
 import Communication1 from "./pages/CommunicationPage1";
@@ -23,32 +24,57 @@ import CommunicationChat4 from "./pages/CommunicationChatPage4.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //npm install react-router-dom
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Start />} />
-        <Route path='/main' element={<Main />} />
-        <Route path='/team1' element={<ContestCheck />} />
-        <Route path='/team2' element={<ContestCheckSchool />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signin' element={<Signin />} />
-        <Route path='/portfolio' element={<PortfolioPage />} />
-        <Route path='/introduce' element={<IntroducePage />} />
-        <Route path='/communication1' element={<Communication1 />} />
-        <Route path='/CommunicationBoard1' element={<CommunicationBoard1 />} />
-        <Route path='/communicationChat1' element={<CommunicationChat1 />} />
-        <Route path='/communication2' element={<Communication2 />} />
-        <Route path='/CommunicationBoard2' element={<CommunicationBoard2 />} />
-        <Route path='/communicationChat2' element={<CommunicationChat2 />} />
-        <Route path='/communication3' element={<Communication3 />} />
-        <Route path='/CommunicationBoard3' element={<CommunicationBoard3 />} />
-        <Route path='/communicationChat3' element={<CommunicationChat3 />} />
-        <Route path='/communication4' element={<Communication4 />} />
-        <Route path='/CommunicationBoard4' element={<CommunicationBoard4 />} />
-        <Route path='/communicationChat4' element={<CommunicationChat4 />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Start />} />
+                <Route path="/main" element={<Main />} />
+                <Route path="/team1" element={<ContestCheck />} />
+                <Route path="/team2" element={<ContestCheckSchool />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/signinbusiness" element={<SigninBusiness />} />
+                <Route path="/portfolio" element={<PortfolioPage />} />
+                <Route path="/introduce" element={<IntroducePage />} />
+                <Route path="/communication1" element={<Communication1 />} />
+                <Route
+                    path="/CommunicationBoard1"
+                    element={<CommunicationBoard1 />}
+                />
+                <Route
+                    path="/communicationChat1"
+                    element={<CommunicationChat1 />}
+                />
+                <Route path="/communication2" element={<Communication2 />} />
+                <Route
+                    path="/CommunicationBoard2"
+                    element={<CommunicationBoard2 />}
+                />
+                <Route
+                    path="/communicationChat2"
+                    element={<CommunicationChat2 />}
+                />
+                <Route path="/communication3" element={<Communication3 />} />
+                <Route
+                    path="/CommunicationBoard3"
+                    element={<CommunicationBoard3 />}
+                />
+                <Route
+                    path="/communicationChat3"
+                    element={<CommunicationChat3 />}
+                />
+                <Route path="/communication4" element={<Communication4 />} />
+                <Route
+                    path="/CommunicationBoard4"
+                    element={<CommunicationBoard4 />}
+                />
+                <Route
+                    path="/communicationChat4"
+                    element={<CommunicationChat4 />}
+                />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
