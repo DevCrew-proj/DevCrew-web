8aba2c54f4ba27b8cf153511543b7d59d5c16ef8
seung yeon <137742302+moanuna@users.noreply.github.com>
2024-08-17 21:36:41 +0900

    feat : API endPoint setting

--- src/pages/CommunicationBoard4.js
+++ src/pages/CommunicationBoard4.js
@@ -10,16 +10,20 @@ const Layout = styled.div`
   height: 1842px;
 `;
 
-const Communication3 = () => {
+const Communication4 = () => {
   return (
     <Layout>
       <Topbar />
       <Listbar3 title='디자인 피드백' showTabs='none' />
-      <FormBoard />
+      <FormBoard 
+        apiEndpoint="https://devcrew.kr/api/v1/feedback/design/create" 
+        fileUploadApiEndpoint="https://devcrew.kr/api/images/design"
+        imageUploadApiEndpoint="https://devcrew.kr/api/images/design"
+      />
       <Bottombar />
     </Layout>
   );
 };
 
-export default Communication3;
+export default Communication4;
 /* <TabBar title="현직자 조언" showTabs={false} /> 탭 아이템이 숨겨짐 */
